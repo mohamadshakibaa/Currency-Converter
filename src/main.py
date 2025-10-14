9b676e985e32c8cd5d55fbf7375d155c08babe1c
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2025-10-14 12:41:52 +0330

    Create a currency converter

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def get_exchange_rate(base_currency, target_currency):
+    url = f"https://v6.exchangerate-api.com/v6/fcbf69001983b522b05841fc/latest/{base_currency}"
+    response = requests.get(url)
+    return response.json()['conversion_rates'][target_currency]
+
+def convert_currency(amount, exchahge_rate):
+    return amount * exchahge_rate
+
+
+if __name__ == "__main__":
+    base_currency = input("Enter your currency: ")
+    target_currency = input("Enter your currency: ")
+    amount = float(input("Enter amount: "))
+    exchange_rate = get_exchange_rate(base_currency, target_currency)
+    converted_amount = convert_currency(amount, exchange_rate)
+    print(f'{amount} {base_currency} is {converted_amount} {target_currency}')
