35a3d2d844ecb6df2977451b4f94290300cdef40
mohamadshakibaa <www.mohamadrasol123@gmail.com>
2025-10-15 19:30:26 +0330

    Add stramlit

---
+++ src/app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from constants import currencies
+from currency_converter import get_exchange_rate, convert_currency
+
+
+st.title(':dollar: Currency Converter')
+
+st.markdown("""
+This tool allows you to instantly convert amounts between different currencies 🌍.
+            
+Enter the amount and choose the currencies to see the result.
+""")
+
+base_currency = st.selectbox('From currency (Base): ', currencies, index=110)
+target_currency = st.selectbox('To currency (Target): ', currencies)
+
+amount = st.number_input('Enter amount: ', min_value=0.0, value=1.0)
+
+if amount > 0 and base_currency and target_currency:
+    exchange_rate = get_exchange_rate(base_currency, target_currency)
+
+    if exchange_rate:
+        converted_amount = convert_currency(amount, exchange_rate)
+        st.success(f"✅ Exchange Rate: {exchange_rate:.3f}")
+        col1, col2, col3 = st.columns(3)
+        col1.metric(label="Base Currency", value=f"{amount:.3f} {base_currency}")
+        col2.markdown("<h1 style='text-align: center; margin: 0; color: blue;'>&#8594;</h1>", unsafe_allow_html=True)
+        col3.metric(label="Target Currency", value=f"{converted_amount:.3f} {target_currency}")
+    else:
+        st.error("Enter fetching exchange rate.")
+
+st.markdown("---")
+st.markdown("### ℹ️ About This Tool")
+st.markdown("""
+This currency converter uses real-time exchange rates provided by the ExchangeRate-API.
+- The conversion updates automatically as you input the amount or change the currency.
+- Enjoy seamless currency conversion without the need to press a button!
+""")
--- src/currency_converter.py
+++ src/currency_converter.py
@@ -4,6 +4,8 @@ import requests
 def get_exchange_rate(base_currency, target_currency):
     url = f"https://v6.exchangerate-api.com/v6/fcbf69001983b522b05841fc/latest/{base_currency}"
     response = requests.get(url)
+    if response.status_code != 200 :
+        return None
     return response.json()['conversion_rates'][target_currency]
 
 def convert_currency(amount, exchahge_rate):
@@ -16,4 +18,5 @@ if __name__ == "__main__":
     amount = float(input("Enter amount: "))
     exchange_rate = get_exchange_rate(base_currency, target_currency)
     converted_amount = convert_currency(amount, exchange_rate)
-    print(f'{amount} {base_currency} is {converted_amount} {target_currency}')
+    print(f'{amount:.3f} {base_currency} is {converted_amount:.3f} {target_currency}')
+    
